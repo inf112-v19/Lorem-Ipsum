2adb73732bf11ee1dfe03d87f100aa1f2ae2a5c5
Henrik <henrik.lg@hotmail.com>
2019-02-08 15:05:03 +0100

    updating IArchive

--- src/main/java/inf112/skeleton/app/Interfaces/IArchive.java
+++ src/main/java/inf112/skeleton/app/Interfaces/IArchive.java
@@ -3,12 +3,20 @@ package inf112.skeleton.app.Interfaces;
 import inf112.skeleton.app.Position;
 
 public interface IArchive {
+	/**
+	 * Interface for backup.
+	 * Classes that implement this interface should call
+	 * players backup methods using the setBackup method.
+	 */
+
 
 
 	/**
-	 * Setting new backup Position for players
+	 * Setting new backup Position for players by
+	 * calling players backup method
+	 * @param player
 	 * @param pos
 	 */
-	void setBackup(Position pos);
+	void setBackup(Player player, Position pos);
 
 }
