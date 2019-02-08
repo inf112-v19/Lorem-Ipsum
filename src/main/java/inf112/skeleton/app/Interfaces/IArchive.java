97aafdb72b57c83f65a8dc0cc3ec9ca721e7d851
Henrik <henrik.lg@hotmail.com>
2019-02-08 14:55:36 +0100

    interface IArchive added

---
+++ src/main/java/inf112/skeleton/app/Interfaces/IArchive.java
@@ -0,0 +1,14 @@
+package inf112.skeleton.app.Interfaces;
+
+import inf112.skeleton.app.Position;
+
+public interface IArchive {
+
+
+	/**
+	 * Setting new backup Position for players
+	 * @param pos
+	 */
+	void setBackup(Position pos);
+
+}
