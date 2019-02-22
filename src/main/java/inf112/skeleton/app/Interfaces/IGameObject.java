deb4d8ec172784603c3def72d61cb4ad26c836a5
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-02-22 13:46:10 +0100

    finished simple IGameObject

--- src/main/java/inf112/skeleton/app/Interfaces/IGameObject.java
+++ src/main/java/inf112/skeleton/app/Interfaces/IGameObject.java
@@ -1,4 +1,23 @@
 package inf112.skeleton.app.Interfaces;
 
+/**
+ * Interface for game objects
+ */
 public interface IGameObject {
+
+    /**
+     * @return The position of the object
+     */
+    Position getPosition();
+
+    /**
+     * Place game object on the board
+     * @param postion
+     */
+    void place(Position postion);
+
+    /**
+     * Removes the object from the board
+     */
+    void remove();
 }
