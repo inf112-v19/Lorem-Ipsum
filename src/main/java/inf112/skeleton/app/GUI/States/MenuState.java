9e60394010f0b7fd1f850d729c4d8678b64c86b9
Emily <eng003@uib.no>
2019-02-22 15:22:53 +0100

    added a picture, ManuStart works but has to be modified

--- src/main/java/inf112/skeleton/app/GUI/States/MenuState.java
+++ src/main/java/inf112/skeleton/app/GUI/States/MenuState.java
@@ -1,11 +1,19 @@
 package inf112.skeleton.app.GUI.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import inf112.skeleton.app.GUI.DemoGame;
 
 public class MenuState extends State {
 
+    Texture background;
+    Texture playButton;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("ok1.png");
+        playButton = new Texture("player.png");
+
     }
 
     @Override
@@ -20,6 +28,10 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, DemoGame.WIDTH, DemoGame.HEIHGT);
+        sb.draw(playButton, (DemoGame.WIDTH / 2) - (playButton.getWidth() / 2), (DemoGame.HEIHGT / 2));
+        sb.end();
     }
 }
+
