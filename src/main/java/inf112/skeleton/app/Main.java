121bd8a4460299c6c9d7f02f4250a74223d2153f
Henrik <henrik.lg@hotmail.com>
2019-02-22 13:18:01 +0100

    Main class made

---
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -0,0 +1,18 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import inf112.skeleton.app.GUI.DemoGame;
+
+public class Main {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Robo Rally";
+		config.width = 576;
+		config.height = 576;
+
+		new LwjglApplication(new DemoGame(), config);
+
+	}
+
+}
