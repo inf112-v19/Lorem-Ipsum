cd4a304df1b249cf171d995f9eb34171d1d78186
Henrik <henrik.lg@hotmail.com>
2019-02-23 19:35:07 +0100

    BoardGUI is now displaying the board in a pleasing way

--- src/main/java/inf112/skeleton/app/GUI/BoardGUI.java
+++ src/main/java/inf112/skeleton/app/GUI/BoardGUI.java
@@ -1,42 +1,71 @@
 package inf112.skeleton.app.GUI;
 
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import static com.badlogic.gdx.Gdx.gl;
+
 
 public class BoardGUI {
 
     Board board;
-    OrthogonalTiledMapRenderer tiledMapRenderer;
+    OrthographicCamera camera;
+    SpriteBatch batch;
 
-    public BoardGUI() {
-        //board = new TmxMapLoader().load("EmptyBoard.tmx");
-        //tiledMapRenderer = new OrthogonalTiledMapRenderer(board);
+
+    int tilesize = (Gdx.graphics.getWidth() + Gdx.graphics.getHeight())/20;
+    int boardWidth = 400;
+    int boardHeight = 400;
+    Texture texture = new Texture("RoboRallyTiles.png");
+    TextureRegion[][] spriteSheet = new TextureRegion(texture,336,624).split(336/7, 624/13);
+
+
+    public BoardGUI(OrthographicCamera camera, SpriteBatch batch) {
+        this.camera = camera;
+        this.batch = batch;
+
     }
 
-    public void render(OrthographicCamera camera, SpriteBatch batch) {
-        //tiledMapRenderer.setView(camera);
-        //tiledMapRenderer.render();
-
+    /**
+     * function that sets the size of the board and tiles so that it is static
+     * calling drawBoard for actuall drawing of the board
+     *
+     * this function is called from RoboRally
+     */
+    public void render() {
+        camera.setToOrtho(false);
         batch.setProjectionMatrix(camera.combined);
+        drawBoard();
+
+    }
+
+
+    /**
+     * function that draws the board using the spriteSheet
+     *
+     */
+    //TODO - should work width different tiles
+    public void drawBoard(){
         batch.begin();
-        //TODO - Draw the board
 
+        for (int i = 0; i < boardHeight; i+= tilesize){
+            for (int j = 0; j < boardWidth; j+= tilesize){
+                batch.draw(spriteSheet[0][4], i, j, tilesize, tilesize);
+            }
+        }
 
-
-        //super.render(camera, batch);
         batch.end();
     }
 
-
-    public void update() {
-
-    }
-
-
-    public void dispose() {
-        //board.dispose();
-    }
 
 }
--- src/main/java/inf112/skeleton/app/GUI/RoboRally.java
+++ src/main/java/inf112/skeleton/app/GUI/RoboRally.java
@@ -2,33 +2,71 @@ package inf112.skeleton.app.GUI;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class RoboRally extends ApplicationAdapter {
     OrthographicCamera camera;
     SpriteBatch batch;
-    BoardGUI gameMap;
+    Viewport viewport;
+    BoardGUI BoardGUI;
+
+    public RoboRally() {
+        super();
+    }
 
     @Override
     public void create () {
         batch = new SpriteBatch();
 
+        //camera = new OrthographicCamera();
+        //camera.setToOrtho(true);
+
+
+
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.update();
 
-        gameMap = new BoardGUI();
+        BoardGUI = new BoardGUI(camera, batch);
     }
 
     @Override
     public void render () {
-        gameMap.update();
-        gameMap.render(camera, batch);
+
+        //TODO - handle input here
+
+        //cleaning screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //redering BoardGUI
+        BoardGUI.render();
+
     }
 
     @Override
     public void dispose () {
         batch.dispose();
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        System.out.println("Resizing");
+
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
     }
 }
