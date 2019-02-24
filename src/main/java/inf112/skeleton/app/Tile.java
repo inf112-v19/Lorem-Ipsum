27b3fcddaabbdc0aab9c9c811969a2383a70ac73
Henrik <henrik.lg@hotmail.com>
2019-02-24 16:08:30 +0100

    added sprite coordinat variables and getters in Tile

--- src/main/java/inf112/skeleton/app/Tile.java
+++ src/main/java/inf112/skeleton/app/Tile.java
@@ -3,7 +3,16 @@ package inf112.skeleton.app;
 import inf112.skeleton.app.Interfaces.ITile;
 
 public abstract class Tile implements ITile {
+	private int spriteX;
+	private int spriteY;
 
+	public int getSpriteX() {
+		return spriteX;
+	}
+
+	public int getSpriteY() {
+		return spriteY;
+	}
 
 	@Override
 	public Position getPosition() {
