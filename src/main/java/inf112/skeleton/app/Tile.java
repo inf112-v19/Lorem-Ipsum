f4500cb3eedc45f3d6cb2e2237f9717b4b401aab
Henrik <henrik.lg@hotmail.com>
2019-02-24 16:41:10 +0100

    GUI changes in iTile and Tile, and made NormalTile class

--- src/main/java/inf112/skeleton/app/Tile.java
+++ src/main/java/inf112/skeleton/app/Tile.java
@@ -3,15 +3,18 @@ package inf112.skeleton.app;
 import inf112.skeleton.app.Interfaces.ITile;
 
 public abstract class Tile implements ITile {
-	private int spriteX;
-	private int spriteY;
 
+	private final int DEFAULT_TILE_SPRITE_X = 4;
+	private final int DEFAULT_TILE_SPRITE_Y = 0;
+
+	@Override
 	public int getSpriteX() {
-		return spriteX;
+		return DEFAULT_TILE_SPRITE_X;
 	}
 
+	@Override
 	public int getSpriteY() {
-		return spriteY;
+		return DEFAULT_TILE_SPRITE_Y;
 	}
 
 	@Override
