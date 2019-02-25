5b278f6381888df29aa875c5a58f4a05f08b47b4
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2019-02-25 19:35:03 +0100

    added images of program cards. probably need later resizing

--- src/main/java/inf112/skeleton/app/Cards/CardType.java
+++ src/main/java/inf112/skeleton/app/Cards/CardType.java
@@ -1,18 +1,18 @@
 package inf112.skeleton.app.Cards;
 
 public enum CardType {
-    FORWARD_1 (0, 1),
-    FORWARD_2 (0, 2),
-    FORWARD_3 (0, 3),
-    BACKWARD_1 (0, -1),
-    ROTATE_180 (2, 0),
-    ROTATE_90_R (1, 0),
-    ROTATE_90_L (-1, 0);
+    BACKWARD_1(0, -1),
+    FORWARD_1(0, 1),
+    FORWARD_2(0, 2),
+    FORWARD_3(0, 3),
+    ROTATE_180(2, 0),
+    ROTATE_90_L(-1, 0),
+    ROTATE_90_R(1, 0);
 
     private final int rotation;
     private final int movement;
 
-    CardType (int rotation, int movement) {
+    CardType(int rotation, int movement) {
         this.rotation = rotation;
         this.movement = movement;
     }
@@ -26,3 +26,5 @@ public enum CardType {
     }
 
 }
+
+//TODO: add sprite for each type
