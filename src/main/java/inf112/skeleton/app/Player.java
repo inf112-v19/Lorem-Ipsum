79c88a858207ca575a0559f311a8376e0433d57b
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-02-25 16:03:17 +0100

    Added setDirection method in player

--- src/main/java/inf112/skeleton/app/Player.java
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -11,25 +11,24 @@ import java.util.ArrayList;
     //TODO:Let player hold its own sprite/picture
     //TODO:Take damage method
     //TODO:Restore health method
-    //
+
 
 
 public class Player {
 
-    private Position playerPostition;
     private String playerID;
 //    private ArrayList<Card> playerHand;
     private Board board;
     private int playerHealth = 5;
 
     String playerDirection; //Direction the player is facing
-    private int direction = 0;  //number used to turn player around
+    private int directionNumber = 0;  //number used to turn player around
 
 
 
     public Player(String playerID, Board board) {
-    this.board = board;
-    this.playerID = playerID;
+        this.board = board;
+        this.playerID = playerID;
     }
 
 
@@ -40,9 +39,9 @@ public class Player {
      */
     public void turnPlayer(int numberOfTurns){
 
-        direction = (direction + numberOfTurns) % 4;
+        directionNumber = (directionNumber + numberOfTurns) % 4;
 
-        switch (direction){
+        switch (directionNumber){
 
             case 0:  playerDirection = "North";
                 break;
@@ -62,15 +61,53 @@ public class Player {
     }
 
     /**
+     * Set players direction
+     * @param direction should be "North", "East", "South" or "West"
+     * @return returns 1 for success or -1 for failed direction change
+     */
+    public int setPlayerDirection(String direction){
+
+        if(direction == "North"){
+            directionNumber = 0;
+            playerDirection = direction;
+
+        }
+        else if(direction == "East"){
+            directionNumber = 1;
+            playerDirection = direction;
+
+        }
+        else if(direction == "South"){
+            directionNumber = 2;
+            playerDirection = direction;
+
+        }
+        else if(direction == "West"){
+            directionNumber = 3;
+            playerDirection = direction;
+
+        }
+        else{
+            return -1;
+        }
+        return 1
+    }
+
+
+//TODO:add a way to move backwards and tell board what way player is facing
+
+    /**
      * Tries to move the player 'numberOfSteps' in the direction the player is facing
      * @param numberOfSteps to move
      */
     public void moveForward(int numberOfSteps) {
-
         for (int i = 0; i < numberOfSteps; i++) {
 
             board.movePlayer(playerID, playerDirection);
         }
     }
+
+
+
 
 }
