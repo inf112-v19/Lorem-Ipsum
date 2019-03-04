a21eec551bcbf5654b6a041271f8563d0e300c6a
Emily <eng003@uib.no>
2019-03-04 14:11:12 +0100

    Added resize method, moved SpriteBatch to child classes and rendered BoardGUI

--- src/main/java/inf112/skeleton/app/GUI/states/PlayState.java
+++ src/main/java/inf112/skeleton/app/GUI/states/PlayState.java
@@ -1,16 +1,27 @@
 package inf112.skeleton.app.GUI.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import inf112.skeleton.app.GUI.BoardGUI;
+import inf112.skeleton.app.GUI.RoboRally;
+import inf112.skeleton.app.GUI.SpriteSheet;
 
 public class PlayState extends State{
 
     private Texture car;
+    private BoardGUI boardGUI;
+    private SpriteBatch batch;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        car = new Texture("player.png");
-        //cam.setToOrtho(false, RoboRally.WIDTH/2, RoboRally.WIDTH / 2);  zoomer inn
+        batch = new SpriteBatch();
+        batch.setProjectionMatrix(camera.combined);
+        boardGUI = new BoardGUI(camera, batch);
+
+        //camera.setToOrtho(false, RoboRally.WIDTH/2, RoboRally.WIDTH / 2); // zoomer inn
     }
 
     @Override
@@ -22,18 +33,18 @@ public class PlayState extends State{
     }
 
     @Override
-    public void render(SpriteBatch sb) {
-
-
-
-
-        //sb.setProjectionMatrix(cam.combined); zoomer inn
-        //sb.begin();
-        //sb.draw(car, 50,50);
-        //sb.end();
+    public void render() {
+        boardGUI.render();
     }
 
     @Override
     public void dispose() {
+    }
+
+    @Override
+    public void resize() {
+        super.resize();
+        batch.setProjectionMatrix(camera.combined);
+        boardGUI.resize();
     }
 }
