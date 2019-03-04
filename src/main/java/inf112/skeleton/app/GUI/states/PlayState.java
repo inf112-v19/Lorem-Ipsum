18058998fdbc7ec6f40a464e8e2256f7365cfb70
Emily <eng003@uib.no>
2019-03-04 14:25:17 +0100

    removed unused code and commented code

--- src/main/java/inf112/skeleton/app/GUI/states/PlayState.java
+++ src/main/java/inf112/skeleton/app/GUI/states/PlayState.java
@@ -1,17 +1,10 @@
 package inf112.skeleton.app.GUI.states;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import inf112.skeleton.app.GUI.BoardGUI;
-import inf112.skeleton.app.GUI.RoboRally;
-import inf112.skeleton.app.GUI.SpriteSheet;
 
 public class PlayState extends State{
 
-    private Texture car;
     private BoardGUI boardGUI;
     private SpriteBatch batch;
 
@@ -20,8 +13,6 @@ public class PlayState extends State{
         batch = new SpriteBatch();
         batch.setProjectionMatrix(camera.combined);
         boardGUI = new BoardGUI(camera, batch);
-
-        //camera.setToOrtho(false, RoboRally.WIDTH/2, RoboRally.WIDTH / 2); // zoomer inn
     }
 
     @Override
