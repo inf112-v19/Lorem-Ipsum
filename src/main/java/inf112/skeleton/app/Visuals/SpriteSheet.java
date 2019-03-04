cf7bd932f104eb90fdfd7801219ccbb6629b217d
Emily <eng003@uib.no>
2019-03-04 21:42:06 +0100

    removed some code to unused assets and put in longer paths were it was modified

--- src/main/java/inf112/skeleton/app/Visuals/SpriteSheet.java
+++ src/main/java/inf112/skeleton/app/Visuals/SpriteSheet.java
@@ -19,7 +19,6 @@ public final class SpriteSheet {
 
 	//Div Sprites
 	private static TextureRegion tapToStartSprite;
-	private static TextureRegion clickAnywhereSprite;
 	private static TextureRegion playerSprite;
 
 	//Menu
@@ -74,25 +73,21 @@ public final class SpriteSheet {
 		this.uTurnSprite = new TextureRegion(texture);
 		//this.uTurnSprite.flip(false, true);
 
-		this.texture = new Texture("tapToStart.gif");
+		this.texture = new Texture("StateImages/tapToStart.gif");
 		this.tapToStartSprite = new TextureRegion(texture);
-		this.tapToStartSprite.flip(false, true);
-
-		this.texture = new Texture("clickAnywhere.png");
-		this.clickAnywhereSprite = new TextureRegion(texture);
-		this.clickAnywhereSprite.flip(false, true);
+		//this.tapToStartSprite.flip(false, true);
 
 		this.texture = new Texture("player.png");
 		this.playerSprite = new TextureRegion(texture);
 		this.playerSprite.flip(false,true);
 
-		this.texture = new Texture("tempStart.jpg");
+		this.texture = new Texture("StateImages/tempBackground.jpg");
 		this.menuBackground = new TextureRegion(texture);
-		//this.menuBackground.flip(false, false);
+		//this.menuBackground.flip(false, true);
 
-		this.texture = new Texture("tapToStart.gif");
+		this.texture = new Texture("StateImages/tapToStart.gif");
 		this.menuPlayButton = new TextureRegion(texture);
-		//this.menuPlayButton.flip(false, false);
+		//this.menuPlayButton.flip(false, true);
 
 		this.texture = new Texture("clear.png");
 		this.cardGuiClearButton = new TextureRegion(texture);
