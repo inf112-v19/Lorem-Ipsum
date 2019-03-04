2bf8f09766f5918f6465ed06e2ab72eaf174e320
Emily <eng003@uib.no>
2019-03-04 14:27:50 +0100

    removed unused code and commented code

--- src/main/java/inf112/skeleton/app/GUI/RoboRally.java
+++ src/main/java/inf112/skeleton/app/GUI/RoboRally.java
@@ -1,11 +1,8 @@
 package inf112.skeleton.app.GUI;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import inf112.skeleton.app.GUI.states.GameStateManager;
 import inf112.skeleton.app.GUI.states.MenuState;
 import inf112.skeleton.app.GUI.states.PlayState;
@@ -17,7 +14,6 @@ public class RoboRally extends ApplicationAdapter {
     public static final String TITLE = "Robo Rally";
 
     private GameStateManager gsm;
-    //private BoardGUI boardGUI;
 
     public RoboRally() {
         super();
@@ -25,38 +21,19 @@ public class RoboRally extends ApplicationAdapter {
 
     @Override
     public void create () {
-        //batch = new SpriteBatch();
-
-
         gsm = new GameStateManager();
-
-        /*
-        camera = new OrthographicCamera();
-        camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); // 0,0 is top left
-        batch.setProjectionMatrix(camera.combined);
-        camera.update();
-        */
-        //boardGUI = new BoardGUI(camera, batch);
-
         gsm.push(new PlayState(gsm));
-
         gsm.push(new MenuState(gsm));
     }
 
     @Override
     public void render () {
-
         //TODO - handle input here
-
         //cleaning screen
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //redering BoardGUI
-        //boardGUI.render();
-
         gsm.update(Gdx.graphics.getDeltaTime());
         gsm.render();
-
     }
 
     @Override
@@ -67,9 +44,7 @@ public class RoboRally extends ApplicationAdapter {
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
-        //boardGUI.resize();
         gsm.resize();
-
     }
 
     @Override
