fcef13d761cf78bad0407cf55448244094bc0ba3
CHaerem <chris.haerem@gmail.com>
2019-03-04 16:45:04 +0100

    Created different tests for Board and Tile

--- src/test/java/inf112/skeleton/app/BoardTest.java
+++ src/test/java/inf112/skeleton/app/BoardTest.java
@@ -2,23 +2,14 @@ package inf112.skeleton.app;
 
 import inf112.skeleton.app.Exceptions.PlayerNotFoundException;
 import inf112.skeleton.app.GameMechanics.Board.Board;
-import inf112.skeleton.app.GameMechanics.Board.BoardBuilder;
 import inf112.skeleton.app.GameMechanics.Direction;
-import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
-import inf112.skeleton.app.GameMechanics.GameObjects.Laser;
 import inf112.skeleton.app.GameMechanics.Player;
 import inf112.skeleton.app.GameMechanics.Position;
-import inf112.skeleton.app.GameMechanics.Tiles.NormalTile;
-import inf112.skeleton.app.GameMechanics.Tiles.Tile;
-import junit.framework.AssertionFailedError;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.util.HashMap;
-
 import static junit.framework.TestCase.assertFalse;
-import static org.junit.Assert.assertArrayEquals;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class BoardTest {
@@ -42,10 +33,60 @@ public class BoardTest {
 	 * Testing that the MovePlayer method gives correct exception when Player is not found
 	 */
 	@Test(expected = PlayerNotFoundException.class)
-	public void boardMovePlayerPlayerNotFoundTest() throws PlayerNotFoundException {
+	public void movePlayerPlayerNotFoundTest() throws PlayerNotFoundException {
 		testBoard.movePlayer(new Player("test", Direction.NORTH), Direction.NORTH);
 	}
 
+	/**
+	 * Testing that the posToPlayer returns the correct player - should player[0] since we know that the position
+	 * of this player is (0, 0)
+	 */
+	@Test
+	public void posToPlayerPlayer0Test() {
+		Player shouldBePlayer0 = testBoard.posToPlayer(new Position(0,0));
+		assertEquals(shouldBePlayer0, players[0]);
+	}
 
+	/**
+	 * Testing that the posToPlayer returns null when given a position with no player present
+	 */
+	@Test
+	public void posToPlayerNullTest() {
+		Player shouldBeNull = testBoard.posToPlayer(new Position(5,5));
+		assertEquals(shouldBeNull, null);
+	}
+
+
+
+	/**
+	 * Testing that the isValidPos works returns false when given a position outside the board
+	 */
+	@Test
+	public void isValidPosFalseTest() {
+		Position outsideBoard = new Position(-1, -1);
+		assertFalse(testBoard.isValidPos(outsideBoard));
+	}
+
+	/**
+	 * Testing that the isValidPos works returns true when given a valid position on the board
+	 */
+	@Test
+	public void isValidPosTrueTest() {
+		Position outsideBoard = new Position(0, 0);
+		assertTrue(testBoard.isValidPos(outsideBoard));
+	}
+
+
+	/**
+	 * Testing that it is possible to move player0 East - should move without any collision or exception
+	 * @throws PlayerNotFoundException
+	 */
+	@Test
+	public void movePlayer0SouthTest() throws PlayerNotFoundException {
+		testBoard.movePlayer(players[0], Direction.EAST);
+		Player shouldBePlayer0 = testBoard.posToPlayer(new Position(1,0));
+
+		assertEquals(shouldBePlayer0, players[0]);
+	}
 
 }
--- src/test/java/inf112/skeleton/app/TileTest.java
+++ src/test/java/inf112/skeleton/app/TileTest.java
@@ -9,10 +9,38 @@ import inf112.skeleton.app.GameMechanics.Tiles.RepairTile;
 import inf112.skeleton.app.GameMechanics.Tiles.Tile;
 import org.junit.Test;
 
+
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TileTest {
+
+	/**
+	 * Testing that the hasGameObject works as expected when finding the object - should return 0(position of
+	 * the GameObject)
+	 */
+	@Test
+	public void hasGameObjectFoundTest(){
+		Tile normalTile = new NormalTile(new GameObject[0], Direction.NORTH);
+		GameObject wall = new Wall(Direction.NORTH);
+
+		normalTile.addGameObject(wall);
+
+		assertEquals(0,normalTile.hasGameObject(wall));
+	}
+
+	/**
+	 * Testing that the hasGameObject works as expected when not finding the object - should return -1
+	 */
+	@Test
+	public void hasGameObjectNotFoundTest(){
+		Tile normalTile = new NormalTile(new GameObject[0], Direction.NORTH);
+		normalTile.addGameObject(new Wall(Direction.NORTH));
+		normalTile.addGameObject(new Wall(Direction.WEST));
+		assertEquals(-1,normalTile.hasGameObject(new Wall(Direction.EAST)));
+	}
 
 	/**
 	 * Testing that the hasWallinDir method works as expected for a tile with a wall North
@@ -31,6 +59,10 @@ public class TileTest {
 	@Test
 	public void hasWallInDirFalseTest() {
 		Tile normalTile = new NormalTile(new GameObject[0], Direction.NORTH);
+		normalTile.addGameObject(new Wall(Direction.NORTH));
+		normalTile.addGameObject(new Wall(Direction.NORTH));
+		normalTile.addGameObject(new Wall(Direction.WEST));
+
 		assertFalse(normalTile.hasWallInDir(Direction.NORTH));
 	}
 }
