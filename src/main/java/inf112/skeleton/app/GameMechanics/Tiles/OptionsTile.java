4fdd3a704942d61baab292678156e1b56d1cb640
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-03-05 14:40:31 +0100

    Made OptionsTile class

--- src/main/java/inf112/skeleton/app/GameMechanics/Tiles/OptionsTile.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/Tiles/OptionsTile.java
@@ -1,4 +1,15 @@
 package inf112.skeleton.app.GameMechanics.Tiles;
 
-public class OptionsTile {
+import inf112.skeleton.app.GameMechanics.Direction;
+import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
+import inf112.skeleton.app.Visuals.SpriteType;
+
+public class OptionsTile extends Tile{
+
+    public OptionsTile(GameObject[] gameObjects, Direction direction) {
+        this.gameObjects = gameObjects;
+        this.direction = direction;
+        super.spriteType = SpriteType.OPTIONS_TILE;
+
+    }
 }
