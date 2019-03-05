cb59f31b65e8e538ec465eb1b6e3f7697c7b0d5b
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-03-05 13:57:54 +0100

    Made simple flagg class

--- src/main/java/inf112/skeleton/app/GameMechanics/GameObjects/Flag.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/GameObjects/Flag.java
@@ -1,6 +1,16 @@
 package inf112.skeleton.app.GameMechanics.GameObjects;
 
+import inf112.skeleton.app.GameMechanics.Direction;
+import inf112.skeleton.app.Visuals.SpriteType;
+
 public class Flag extends GameObject {
 
+    private SpriteType spriteType;
+    public Flag(Direction dir){
+        super(dir);
+    }
 
+    public SpriteType getSpriteType(){
+        return spriteType;
+    }
 }
