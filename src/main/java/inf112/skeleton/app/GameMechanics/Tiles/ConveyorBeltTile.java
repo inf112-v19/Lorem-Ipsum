5ef6feebdfeb00d5c501299c86b8c91fc88d9306
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-03-05 15:27:46 +0100

    made a simple conveyorBeltTile with only simple directions

--- src/main/java/inf112/skeleton/app/GameMechanics/Tiles/ConveyorBeltTile.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/Tiles/ConveyorBeltTile.java
@@ -1,4 +1,35 @@
 package inf112.skeleton.app.GameMechanics.Tiles;
 
-public class ConveyorBeltTile {
+import inf112.skeleton.app.GameMechanics.Direction;
+import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
+import inf112.skeleton.app.Visuals.SpriteType;
+
+public class ConveyorBeltTile extends Tile {
+
+    public ConveyorBeltTile(GameObject[] gameObjects, Direction direction) {
+        this.gameObjects = gameObjects;
+        this.direction = direction;
+        setDirection();
+    }
+
+    private void setDirection(){
+        switch (direction){
+            case NORTH:
+                super.spriteType = SpriteType.CONVEYOR_BELT_TILE_NORTH;
+                break;
+            case SOUTH:
+                spriteType = SpriteType.CONVEYOR_BELT_TILE_SOUTH;
+                break;
+            case EAST:
+                spriteType = SpriteType.CONVEYOR_BELT_TILE_EAST;
+                break;
+            case WEST:
+                spriteType = SpriteType.CONVEYOR_BELT_TILE_WEST;
+                break;
+            default:
+                System.err.println("No valid Direction in ConveyorBelt!");
+                break;
+        }
+    }
+
 }
