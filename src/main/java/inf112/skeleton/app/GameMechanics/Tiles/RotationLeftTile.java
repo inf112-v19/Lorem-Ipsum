99dfbb7a112d96c5a8773db852999aa90952b6d1
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-03-06 16:30:51 +0100

    Finished RotationLeftTile

--- src/main/java/inf112/skeleton/app/GameMechanics/Tiles/RotationLeftTile.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/Tiles/RotationLeftTile.java
@@ -1,4 +1,15 @@
 package inf112.skeleton.app.GameMechanics.Tiles;
 
-public class RotationLeftTile {
+import inf112.skeleton.app.GameMechanics.Direction;
+import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
+import inf112.skeleton.app.Visuals.SpriteType;
+
+public class RotationLeftTile extends Tile{
+
+    public RotationLeftTile(GameObject[] gameObjects, Direction direction) {
+        this.gameObjects = gameObjects;
+        this.direction = direction;
+        super.spriteType = SpriteType.ROTATION_LEFT_TILE;
+
+    }
 }
