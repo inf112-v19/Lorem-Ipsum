c98372ff133b766a54c6c8d93b3b663e8d297435
Emily <eng003@uib.no>
2019-03-07 12:46:37 +0100

    refactored from pushedStateWillBeOnTopOfStack to popStateTest and actionStaten to actionState

--- src/test/java/inf112/skeleton/app/StateTest.java
+++ src/test/java/inf112/skeleton/app/StateTest.java
@@ -1,5 +1,6 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.math.Interpolation;
 import inf112.skeleton.app.GameMechanics.Board.Board;
 import inf112.skeleton.app.Visuals.States.ActionState;
 import inf112.skeleton.app.Visuals.States.GameStateManager;
@@ -7,6 +8,8 @@ import inf112.skeleton.app.Visuals.States.MenuState;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.lwjgl.Sys;
+
 import static org.junit.Assert.*;
 
 @RunWith(GdxTestInit.class)
@@ -18,16 +21,17 @@ public class StateTest {
 	public void setUp() throws Exception {
 		gsm = new GameStateManager();
 		board = new Board("Boards/ExampleBoard.txt");
-
 	}
 
 	@Test
-	public void pushedStateWillBeOnTopOfStack() {
-		ActionState actionStaten = new ActionState(gsm,board);
+	public void popStateTest() {
+		ActionState actionState = new ActionState(gsm,board);
 		gsm.push(new MenuState(gsm, board));
-		gsm.push(actionStaten);
-		assertSame(gsm.pop(), actionStaten);
+		gsm.push(actionState);
+		assertSame(gsm.pop(), actionState);
 	}
+
+
 
 
 }
