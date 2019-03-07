f09d1d5c96ba4693bd7f82d8fbae63d5c613bed2
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-03-07 12:56:06 +0100

    made checktile for NormalTile

--- src/main/java/inf112/skeleton/app/GameMechanics/Tiles/NormalTile.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/Tiles/NormalTile.java
@@ -1,6 +1,10 @@
 package inf112.skeleton.app.GameMechanics.Tiles;
 
+import inf112.skeleton.app.Exceptions.PlayerNotFoundException;
+import inf112.skeleton.app.GameMechanics.Board.Board;
 import inf112.skeleton.app.GameMechanics.Direction;
+import inf112.skeleton.app.GameMechanics.GameObjects.Flag;
+import inf112.skeleton.app.GameMechanics.Player;
 import inf112.skeleton.app.Visuals.SpriteType;
 import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
 
@@ -10,7 +14,17 @@ public class NormalTile extends Tile {
 		this.gameObjects = gameObjects;
 		this.direction = direction;
 		super.spriteType = SpriteType.NORMAL_TILE;
-
 	}
 
+
+	@Override
+	public void checkTile(Board board, Player player) throws PlayerNotFoundException {
+		for(int i=0;i<gameObjects.length;i++){
+			if(gameObjects[i] instanceof Flag){
+				player.increaseHealth();
+				player.setBackup(board.getPlayerPos(player));
+				break;
+			}
+		}
+	}
 }
