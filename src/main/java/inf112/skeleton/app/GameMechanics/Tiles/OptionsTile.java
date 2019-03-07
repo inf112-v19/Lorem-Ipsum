5fe7ca1af27910212c51a65263273d12e21331e4
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-03-07 13:35:02 +0100

    added checkTile method to optionsTile

--- src/main/java/inf112/skeleton/app/GameMechanics/Tiles/OptionsTile.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/Tiles/OptionsTile.java
@@ -1,7 +1,10 @@
 package inf112.skeleton.app.GameMechanics.Tiles;
 
+import inf112.skeleton.app.Exceptions.PlayerNotFoundException;
+import inf112.skeleton.app.GameMechanics.Board.Board;
 import inf112.skeleton.app.GameMechanics.Direction;
 import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
+import inf112.skeleton.app.GameMechanics.Player;
 import inf112.skeleton.app.Visuals.SpriteType;
 
 public class OptionsTile extends Tile{
@@ -10,6 +13,11 @@ public class OptionsTile extends Tile{
         this.gameObjects = gameObjects;
         this.direction = direction;
         super.spriteType = SpriteType.OPTIONS_TILE;
+    }
 
+    @Override
+    public void checkTile(Board board, Player player) throws PlayerNotFoundException {
+        player.increaseHealth();
+        player.setBackup(board.getPlayerPos(player));
     }
 }
