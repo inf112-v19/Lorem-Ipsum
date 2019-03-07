b6ab3a288b87eb0650603b5ee51bdc3a13e02d84
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-03-07 13:26:55 +0100

    made checkTile for RepairTile

--- src/main/java/inf112/skeleton/app/GameMechanics/Tiles/RepairTile.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/Tiles/RepairTile.java
@@ -1,6 +1,9 @@
 package inf112.skeleton.app.GameMechanics.Tiles;
 
+import inf112.skeleton.app.Exceptions.PlayerNotFoundException;
+import inf112.skeleton.app.GameMechanics.Board.Board;
 import inf112.skeleton.app.GameMechanics.Direction;
+import inf112.skeleton.app.GameMechanics.Player;
 import inf112.skeleton.app.Visuals.SpriteType;
 import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
 
@@ -12,4 +15,10 @@ public class RepairTile extends Tile {
 		super.spriteType = SpriteType.REPAIR_TILE;
 	}
 
+	@Override
+	public void checkTile(Board board, Player player) throws PlayerNotFoundException {
+		player.increaseHealth();
+		player.increaseHealth();
+		player.setBackup(board.getPlayerPos(player));
+	}
 }
