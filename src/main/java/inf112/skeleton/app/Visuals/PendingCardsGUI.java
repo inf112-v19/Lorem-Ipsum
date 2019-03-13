87686f939383e720eeb0d2d5c4072afcbb0b5b33
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2019-03-13 12:46:41 +0100

    added empty PendingCardGUI

---
+++ src/main/java/inf112/skeleton/app/Visuals/PendingCardsGUI.java
@@ -0,0 +1,11 @@
+package inf112.skeleton.app.Visuals;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import inf112.skeleton.app.GameMechanics.Board.Board;
+
+public class PendingCardsGUI {
+    public PendingCardsGUI(OrthographicCamera camera, SpriteBatch batch, Board board) {
+
+    }
+}
