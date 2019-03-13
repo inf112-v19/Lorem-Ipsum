551cc8f8f35b44ee444e33965d88a2e3f33ec862
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2019-03-13 13:38:49 +0100

    added constructor on PendingCardsGUI

--- src/main/java/inf112/skeleton/app/Visuals/PendingCardsGUI.java
+++ src/main/java/inf112/skeleton/app/Visuals/PendingCardsGUI.java
@@ -1,11 +1,22 @@
 package inf112.skeleton.app.Visuals;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import inf112.skeleton.app.GameMechanics.Board.Board;
 
 public class PendingCardsGUI {
-    public PendingCardsGUI(OrthographicCamera camera, SpriteBatch batch, Board board) {
 
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Board board;
+
+    private BitmapFont font;
+    private String stringFont;
+
+    public PendingCardsGUI(OrthographicCamera camera, SpriteBatch batch, Board board) {
+        this.camera = camera;
+        this.batch = batch;
+        this.board = board;
     }
 }
