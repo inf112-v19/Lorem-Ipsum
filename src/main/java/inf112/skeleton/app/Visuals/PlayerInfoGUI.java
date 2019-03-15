cf21cc7238d1609ea339433658031bbd9ebd99c9
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2019-03-15 23:49:29 +0100

    added empty class PlayerInfoGUI. will display health and lives for each player

---
+++ src/main/java/inf112/skeleton/app/Visuals/PlayerInfoGUI.java
@@ -0,0 +1,15 @@
+package inf112.skeleton.app.Visuals;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class PlayerInfoGUI {
+    private BitmapFont[] playerNames;
+
+    private Stage stage;
+
+
+    private void drawHealthPoint(int x, int y) {
+        
+    }
+}
