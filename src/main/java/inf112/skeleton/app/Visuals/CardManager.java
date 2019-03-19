1e3a34ecb9d62ab2c447f79f7a8c47f4aa2e7671
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2019-03-19 14:16:49 +0100

    added CardManager class and constructor

--- src/main/java/inf112/skeleton/app/Visuals/CardManager.java
+++ src/main/java/inf112/skeleton/app/Visuals/CardManager.java
@@ -1,4 +1,25 @@
 package inf112.skeleton.app.Visuals;
 
-public class CardManager {
-}
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import inf112.skeleton.app.GameMechanics.Board.Board;
+import inf112.skeleton.app.Interfaces.ICardDeck;
+
+    public class CardManager {
+
+        private ICardDeck cardDeck;
+
+        private Board board;
+        private OrthographicCamera camera;
+        private Batch batch;
+
+        public CardManager(Board board, OrthographicCamera camera, Batch batch) {
+            this.board = board;
+            this.camera = camera;
+            this.batch = batch;
+        }
+
+
+    }
+
