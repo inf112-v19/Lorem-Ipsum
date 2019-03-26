721fb5bc0bc5df9d2cde1414c56bdfe3ec98dca5
CHaerem <chris.haerem@gmail.com>
2019-03-26 17:55:31 +0100

    Removed unused methods from board and added method for placing a flag on a tile

--- src/main/java/inf112/skeleton/app/Interfaces/IBoard.java
+++ src/main/java/inf112/skeleton/app/Interfaces/IBoard.java
@@ -3,6 +3,7 @@ package inf112.skeleton.app.Interfaces;
 import inf112.skeleton.app.Exceptions.PlayerNotFoundException;
 import inf112.skeleton.app.GameMechanics.Cards.Card;
 import inf112.skeleton.app.GameMechanics.Direction;
+import inf112.skeleton.app.GameMechanics.GameObjects.Flag;
 import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
 import inf112.skeleton.app.GameMechanics.Player;
 import inf112.skeleton.app.GameMechanics.Position;
@@ -26,28 +27,6 @@ public interface IBoard<T> {
 	 */
 	Player[] getAllPlayers();
 
-    /**
-     * Set the Game Object on the tile in the given Position.
-     *
-     * @param pos
-     *            given position you want to set the object
-     * @param gameObject
-     *            the Game Object being placed
-     * @throws IndexOutOfBoundsException
-     *             if !isValid(x,y)
-     */
-    void setGameObject(Position pos, GameObject gameObject);
-
-    /**
-     * Get the Game Objects on the tile in the given Position.
-     *
-     * @param pos
-     *            position to the objects
-     * @return the object that you are getting
-     * @throws IndexOutOfBoundsException
-     *             if !isValid(x,y)
-     */
-	GameObject[] getGameObject(Position pos);
 
 	/**
 	 * Tries to move the player in the given direction numberOfMoves times by calling the
@@ -78,20 +57,6 @@ public interface IBoard<T> {
      */
     boolean movePlayer(Player player, Direction dir) throws PlayerNotFoundException;
 
-    /**
-     * Remove a Game Object from the tile at the given x,y location.
-     *
-     * y must be greater than or equal to 0 and less than getHeight(). x must be
-     * greater than or equal to 0 and less than getWidth().
-     *
-     * @param pos
-     *            remove the object in this position
-     * @param gameObject
-     *            An element to be removed from the tile
-     * @throws IndexOutOfBoundsException
-     *             if !isValid(x,y)
-     */
-    void removeObject(Position pos, GameObject gameObject);
 
     /**
      * Check if coordinates are valid.
@@ -153,4 +118,13 @@ public interface IBoard<T> {
 	 * @return
 	 */
 	Card getCurCard();
+
+	/**
+	 * Tries to add a flag to the tile if it does noe already contain a flag
+	 *
+	 * @param flag
+	 * @param tile
+	 * @return true if the flag was placed on the tile, or false if not
+	 */
+	boolean placeFlagOnTile(Flag flag, Tile tile);
 }
