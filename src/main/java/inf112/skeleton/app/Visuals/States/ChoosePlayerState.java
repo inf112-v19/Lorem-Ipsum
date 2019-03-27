3048e5993cdef3d89867b79fafa8c30b2ec015f8
emilymnl <eng003@uib.no>
2019-03-27 17:16:18 +0100

    deleted import to textfield

--- src/main/java/inf112/skeleton/app/Visuals/States/ChoosePlayerState.java
+++ src/main/java/inf112/skeleton/app/Visuals/States/ChoosePlayerState.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import inf112.skeleton.app.GameMechanics.Board.Board;
