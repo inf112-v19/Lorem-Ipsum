5a1bc5be96b9709218a4a5723418f7c2162e28cb
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-03-28 14:33:51 +0100

    Changed destroyPlayer in player to set playerhealth to different amount depending on number of lost lives

--- src/main/java/inf112/skeleton/app/GameMechanics/Player.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/Player.java
@@ -144,15 +144,22 @@ public class Player extends Image implements IPlayer {
     }
 
     /**
-     * destroy the player (lose a total life and set health to max) or if no more lives set health to 0
+     * destroy the player (lose a total life and set health to the right amount) or if no more lives set health to 0
      * and remove player from the board
      */
     public void destroyPlayer() {
         playerlives--;
 
-        if (playerlives > 0) {
+        if (playerlives > 2) {
             playerHealth = 10;
-        } else {
+        }
+        else if(playerlives == 1){
+            playerHealth = 8;
+        }
+        else if(playerlives == 0){
+            playerHealth = 6;
+        }
+        else {
             playerHealth = 0;
             isOnTheBoard = false;
         }
