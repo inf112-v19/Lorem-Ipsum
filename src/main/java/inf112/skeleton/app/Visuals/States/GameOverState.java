8513d2032a3e211e44ed8034bd4ea954f7dc02e5
emilymnl <eng003@uib.no>
2019-03-31 01:31:13 +0100

    added super.render()

--- src/main/java/inf112/skeleton/app/Visuals/States/GameOverState.java
+++ src/main/java/inf112/skeleton/app/Visuals/States/GameOverState.java
@@ -76,6 +76,7 @@ public class GameOverState extends State {
 
     @Override
     public void render() {
+        super.render();
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         this.stage.draw();
