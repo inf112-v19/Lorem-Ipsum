47bc23f62b2de94e68aac23c70ce1271555a81eb
emilymnl <eng003@uib.no>
2019-03-31 01:32:30 +0100

    removed field variable gsm (!!) every button works now but not exactly the keypressed to close the pause again

--- src/main/java/inf112/skeleton/app/Visuals/States/PauseState.java
+++ src/main/java/inf112/skeleton/app/Visuals/States/PauseState.java
@@ -22,11 +22,9 @@ import inf112.skeleton.app.Visuals.SpriteSheet;
 
 
 public class PauseState extends State{
-    GameStateManager gsm;
-    SpriteSheet spriteSheet;
-    //Stage stage;
 
     //boolean
+
     private boolean mainMenu;
     private boolean resume;
     private boolean exit;
@@ -36,6 +34,8 @@ public class PauseState extends State{
     private Image resumeButton;
     private Image mainMenuButton;
     private Image exitButton;
+
+    private String buttonName;
 
 
     public PauseState(GameStateManager gsm) {
@@ -62,36 +62,32 @@ public class PauseState extends State{
         this.resumeButton.setSize(191, 49);
         this.resumeButton.setPosition((RoboRally.WIDTH/2)-(191/2), RoboRally.HEIGHT-(49*3));
         this.stage.addActor(this.resumeButton);
-        clickable(resumeButton, "resume");
-        this.resume = true;
+        clickable(this.resumeButton, "resume");
     }
 
     private void setMainMenu() {
         this.mainMenuButton.setSize(191, 49);
         this.mainMenuButton.setPosition((RoboRally.WIDTH/2)-(191/2), RoboRally.HEIGHT-(49*5));
         this.stage.addActor(this.mainMenuButton);
-        clickable(mainMenuButton, "mainMenu");
+        clickable(this.mainMenuButton, "mainMenu");
     }
 
     private void setExit() {
         this.exitButton.setSize(191, 49);
         this.exitButton.setPosition((RoboRally.WIDTH/2)-(191/2), RoboRally.HEIGHT-(49*7));
         this.stage.addActor(this.exitButton);
-        clickable(exitButton, "exit");
+        clickable(this.exitButton, "exit");
     }
 
-    private void clickable(Image boardType, final String boardName) {
-        boardType.addListener(new InputListener() {
+    private void clickable(Image buttonType, final String buttonName) {
+        buttonType.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                //System.out.println("Board " + boardName + " was chosen!");
-                System.out.println("clickeddddd");
-                //saveBoardName(boardName);
-                if (boardName.equals("resume")) {
+                if (buttonName.equals("resume")) {
                     resume = true;
-                } if (boardName.equals("mainMenu")) {
+                } else if (buttonName.equals("mainMenu")) {
                     mainMenu = true;
-                } if (boardName.equals("exit")) {
+                } else if (buttonName.equals("exit")) {
                     exit = true;
                 }
                 start = true;
@@ -104,69 +100,36 @@ public class PauseState extends State{
     @Override
     protected void handleInput() {
         if (this.start) {
-            /*if (this.mainMenu) {
+            if (this.resume) {
+                System.out.println("Resume Game");
+                this.gsm.pop();
+            } else if (this.mainMenu) {
                 System.out.println("New Game!");
-                //gsm.set(new MenuState(gsm));
-                //dispose();
-            }*/
-        /*if (Gdx.input.isKeyPressed(Input.Keys.P) || this.resume) {
-            System.out.println("Resume Game");
-            //tilbake til spillet
-        }*/
-            if (Gdx.input.justTouched()) {
+                this.gsm.pop();
+                this.gsm.set(new MenuState(this.gsm));
+                dispose();
+            } else if (this.exit) {
                 System.out.println("Exit Game!");
-                //terminere spillet
                 Gdx.app.exit();
-
             }
-        }
 
+        }
     }
 
     @Override
     public void update(float dt) {
         handleInput();
     }
-/*
-    private void test() {
-        Table table = new Table();
-        Window.WindowStyle style = new Window.WindowStyle(new BitmapFont(), Color.WHITE);
-        Window pause = new Window("Paused", style);
-        pause.setMoveable(false); //So the user can't move the window
-        pause.add(new TextButton("Unpause", skin)); //Add a new text button that unpauses the game.
-        pause.pack(); //Important! Correctly scales the window after adding new elements.
-        float newWidth = 400, newHeight = 200;
-        pause.setBounds((Gdx.graphics.getWidth() - newWidth ) / 2,
-                (Gdx.graphics.getHeight() - newHeight ) / 2, newWidth , newHeight ); //Center on screen.
-        stage.addActor(pause);
-    }*/
 
     @Override
     public void render() {
         super.render();
-        /*TextureRegion textBar = new TextureRegion(new Texture("StateImages/.png"));
-        //TextureRegion background = this.spriteSheet.getTexture(SpriteType.MENU_BACKGROUND);
-        this.stage.act();
-        this.stage.getBatch().begin();
-        this.stage.getBatch().draw(textBar, 0, 0, RoboRally.WIDTH, RoboRally.HEIGHT);
-        this.stage.getBatch().end();
-        */
-        /*Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        */
-        //this.stage.draw();
-
     }
 
     @Override
     public void resize() {
-        stage.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        super.resize();
+        this.stage.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
-
-    @Override
-    public void dispose() {
-        super.dispose();
-    }
-
 
 }
