437d38f9dfe6d210798acea5f20956eb038d434d
CHaerem <chris.haerem@gmail.com>
2019-04-14 19:13:51 +0200

    Improved toggleLaser method to skip adding laser to laser base tile when toggling since lasers already are present on the laser base texture

--- src/main/java/inf112/skeleton/app/GameMechanics/Tiles/LaserBaseTile.java
+++ src/main/java/inf112/skeleton/app/GameMechanics/Tiles/LaserBaseTile.java
@@ -1,9 +1,11 @@
 package inf112.skeleton.app.GameMechanics.Tiles;
 
+import inf112.skeleton.app.GameMechanics.Board.Board;
 import inf112.skeleton.app.GameMechanics.Direction;
 import inf112.skeleton.app.GameMechanics.GameObjects.GameObject;
 import inf112.skeleton.app.GameMechanics.GameObjects.Laser;
 import inf112.skeleton.app.GameMechanics.GameObjects.Wall;
+import inf112.skeleton.app.GameMechanics.Position;
 import inf112.skeleton.app.Visuals.SpriteType;
 
 public class LaserBaseTile extends Tile {
@@ -29,6 +31,16 @@ public class LaserBaseTile extends Tile {
 				super.spriteType = SpriteType.LASERBASE_WEST;
 				break;
 			default: //will never happen since direction is an enum
+		}
+	}
+
+	@Override
+	public void toggleLaser(Position pos, Board board, boolean laserStatus) {
+		int possibleToMoveDir = this.isPossibleToMoveDir(pos, board, this.direction);
+
+		//skips adding laser to the base since its already drawn as part of the texture
+		if (possibleToMoveDir == 0 || possibleToMoveDir == 2) {
+			super.toggleLaser(pos, board, laserStatus);
 		}
 	}
 }
