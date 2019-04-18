06e039c2f571218d2fb0d6e8a7a8101ef01c77ba
HeLiHex <henrik.lh@hotmail.com>
2019-04-18 13:56:39 +0200

    Adding two new buttons in the MenuState table. One "host a game" button and one "join a game" button

--- src/main/java/inf112/skeleton/app/Visuals/States/MenuState.java
+++ src/main/java/inf112/skeleton/app/Visuals/States/MenuState.java
@@ -1,33 +1,40 @@
 package inf112.skeleton.app.Visuals.States;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import inf112.skeleton.app.Visuals.RoboRally;
 
 public class MenuState extends State {
 	private TextureRegionDrawable background;
 	private Image startButton;
+	private TextButton hostGameButton;
+	private TextButton joinGameButton;
 	private Table table;
 
 	private boolean start;
+	private boolean isHostingGame;
+	private boolean isJoiningGame;
 
 	public MenuState(GameStateManager gsm) {
 		super(gsm);
 
+		this.start = false;
+		this.isHostingGame = false;
+		this.isJoiningGame = false;
+
 		this.table = new Table();
 		this.table.setFillParent(true);
+		this.table.defaults().uniform();
 		this.table.center();
-
-		this.start = false;
 
 		this.background = new TextureRegionDrawable(super.assetHandler.getTexture("StateImages/menuBackground.jpg"));
 		this.table.setBackground(this.background);
 
 		setStartButton();
+		setHostGameButton();
+		setJoinGameButton();
 
 		super.stage.addActor(table);
 	}
@@ -35,8 +42,45 @@ public class MenuState extends State {
 	private void setStartButton() {
 		this.startButton = new Image(assetHandler.getTextureRegion("StateImages/start.png"));
 		this.table.add(startButton);
+		this.table.row();
 
 		clickable(this.startButton);
+	}
+
+	private void setHostGameButton(){
+		this.hostGameButton = new TextButton("Host a game", assetHandler.getSkin());
+		this.hostGameButton.getLabel().setFontScale(1.5f);
+		this.hostGameButton.getLabel().setColor(Color.WHITE);
+		this.hostGameButton.setColor(Color.BROWN);
+		this.table.add(hostGameButton);
+		this.table.row();
+
+		this.hostGameButton.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("Host game!");
+				isHostingGame = true;
+				return true;
+			}
+		});
+	}
+
+	private void setJoinGameButton(){
+		this.joinGameButton = new TextButton("Join a game", assetHandler.getSkin());
+		this.joinGameButton.getLabel().setFontScale(1.5f);
+		this.joinGameButton.getLabel().setColor(Color.WHITE);
+		this.joinGameButton.setColor(Color.BROWN);
+		this.table.add(joinGameButton);
+		this.table.row();
+
+		this.hostGameButton.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("Join Game!");
+				isJoiningGame = true;
+				return true;
+			}
+		});
 	}
 
 	private void clickable(Image button) {
@@ -54,6 +98,11 @@ public class MenuState extends State {
 	public void handleInput() {
 		if (this.start) {
 			gsm.set(new ChooseBoardState(gsm));
+		}else if(this.isHostingGame){
+			System.out.println("Hosting a game!");
+			gsm.set(new ChooseBoardState(gsm));
+		}else if(this.isJoiningGame){
+			System.out.println("joining a game");
 		}
 	}
 
