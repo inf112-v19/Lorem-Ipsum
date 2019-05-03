8adb7ce8730cb5710ef0d872ead0145b4e75234e
CHaerem <chris.haerem@gmail.com>
2019-05-03 14:32:46 +0200

    Removed print causing errors

--- src/main/java/inf112/skeleton/app/Visuals/States/PlayerNameState.java
+++ src/main/java/inf112/skeleton/app/Visuals/States/PlayerNameState.java
@@ -256,7 +256,6 @@ public class PlayerNameState extends State {
 		super.update(dt);
 		if (this.host != null && this.clientNames == null){
 			this.clientNames = this.host.getHostHandler().getNames();
-			System.out.println(this.clientNames.size());
 		}
 	}
 }
